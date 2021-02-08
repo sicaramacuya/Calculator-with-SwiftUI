b22929acce0d44a534ef7ac5c577299ec5c90d4c
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-02-08 19:07:42 -0400

    Initial Commit

---
+++ Calculator with SwiftUI/Calculator_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_with_SwiftUIApp.swift
+//  Calculator with SwiftUI
+//
+//  Created by Eric Morales on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
